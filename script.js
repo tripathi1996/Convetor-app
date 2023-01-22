4d57bf7df0ddd151c75863d95929924812c7d56b
shubham tripathi <117027695+tripathi1996@users.noreply.github.com>
2023-01-22 17:20:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-const from = document.getElementById('from');
-const to = document.getElementById('to');
-const inputNumber = document.getElementById('input-number');
-const resultBox = document.getElementById('result-box');
-document.getElementById('convert').addEventListener('click', () => {
-    let fromValue = from.value;
-    let toValue = to.value;
-    let inputValue = inputNumber.value;
-    let result = convertBase(fromValue, toValue, inputValue);
-    resultBox.innerText = result;
-});
+// Access the values of the elements
+
+let from = document.getElementById("from") //for selecting --> from option
+let to = document.getElementById("to") //for selecting --> to option
+let inputNumber = document.getElementById("input-number") //for selecting --> Input value
+let resultBox = document.getElementById("result-box") //for selecting --> Output value
+let reset = document.getElementById("reset"); //for selecting --> Reset Button
+
+
+
+document.getElementById("convert").addEventListener("click", () => {
+
+    let fromValue = from.value //check from
+
+    let toValue = to.value //check to
+
+    let inputValue = inputNumber.value //check input no
+
+    let result = convertBase(fromValue, toValue, inputValue) //convert input no "from" to "to"
+
+    resultBox.innerText = result //show result in result box
+})
 
 function convertBase(fromBase, toBase, num) {
-    let d = parseInt(num, fromBase);
-    let res = d.toString(toBase);
-    return res;
+    let d = parseInt(num, fromBase) //converting no into base10
+    let res = d.toString(toBase) //converting base10 to required base
+    return res
 }
 
-// swapping
-document.getElementById('swap').addEventListener('click', () => {
-    let temp = from.value;
-    from.value = to.value;
-    to.value = temp;
+
+//---------------------------------------------------------------------------------------
+
+//Adding Event on Swap Button
+
+document.getElementById("swap").addEventListener("click", () => {
+
+    let temp = from.value //grab value from "from"
+    from.value = to.value
+    to.value = temp
+})
+
+//--------------------------------------------------------------------------------------
+
+//Adding Event on Reset Button
+
+reset.addEventListener("click", () => {
+    inputNumber.value = "";
+    resultBox.value = "";
+
+
 });
+
+//For Day and Night theme
+
+var content = document.getElementsByTagName('body')[0];
+var darkMode = document.getElementById('dark-change');
+darkMode.addEventListener('click', function() {
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+})
